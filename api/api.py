90729e45fd9ae28fa950cd0340a49f8198461651
AtheeshThanaseelan <a10@live.ca>
2020-07-19 19:31:18 -0400

    File Upload on React

--- api/api.py
+++ api/api.py
@@ -2,26 +2,27 @@ import os
 from flask import Flask, flash, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
 from flask_cors import CORS, cross_origin
-import logging
+#import logging
 
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
-logger = logging.getLogger('HELLO WORLD')
+#logger = logging.getLogger('HELLO WORLD')
 
 
 
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = 'ok'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = b'ok'
 
 @app.route('/upload', methods=['POST'])
 def fileUpload():
     target=os.path.join(UPLOAD_FOLDER,'test_docs')
     if not os.path.isdir(target):
         os.mkdir(target)
-    logger.info("welcome to upload`")
+    #logger.info("welcome to upload`")
     file = request.files['file'] 
     filename = secure_filename(file.filename)
     destination="/".join([target, filename])
@@ -34,4 +35,4 @@ if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True,host="0.0.0.0",use_reloader=False)
 
-flask_cors.CORS(app, expose_headers='Authorization')
+CORS(app, expose_headers='Authorization')
